3d8400a2a3c696c9acde06413104cdff827e3aa5
Igor Leahu <igor.leahu24@gmail.com>
2017-02-15 08:29:16 +0200

    i_matrix -> a_matrix & a_list

--- public/app.js
+++ public/app.js
@@ -3,80 +3,21 @@ app.config(($mdThemingProvider) => {
 	$mdThemingProvider.theme('default')
 		.dark()
 		.primaryPalette('yellow')
-		.accentPalette('green');
+		.accentPalette('red');
 });
 
 app.controller('DeskCtrl', ($scope) => {
-	let working = false;
 	$scope.input_types = {
 		list: [
 			{
 				name: 'Matricea de incidență',
-				src: '/parts/adjacency_matrix.html',
-				watch: (n, o) => {
-					if (working) return;
-					working = true;
-					// $scope.data.i_matrix = [];
-					n.forEach((row) => {
-						// console.log(row)
-					});
-
-					working = false;
-				}
+				src: '/parts/incidence_matrix.html',
 			}, {
 				name: 'Matricea de adiacență',
-				src: '/parts/incidence_matrix.html',
-				generate: () => {
-					let val = $scope.points.count;
-					$scope.data.i_matrix = Array.apply(null, new Array(val)).map(() => Array.apply(null, new Array(val)).map(() => rand(0, 1)));
-				},
-				watch: (n, o) => {
-					if (working) return;
-					working = true;
-					$scope.data.a_list = [];
-					n.forEach((row) => {
-						let f = [];
-						row.forEach((item, j) => {
-							if (item == 1)
-								f.push(j + 1)
-						});
-						f.sort();
-						$scope.data.a_list.push(f);
-					});
-
-					$scope.data.a_matrix = [];
-					$scope.data.a_list.forEach((row, rowIndex) => {
-						let f = [];
-						let add = false;
-
-						for (var i = 1; i <= $scope.points.count; i++) {
-							let v = row.indexOf(i) != -1 ? 1 : 0;
-							let g = v;
-							if (v) add = true;
-							f.push(g)
-						}
-						$scope.data.a_matrix.push(f)
-					});
-
-					working = false;
-				}
+				src: '/parts/adjacency_matrix.html',
 			}, {
 				name: 'Lista de adiacență',
-				src: '/parts/adjacency_list.html',
-				watch: (n, o) => {
-					if (working) return;
-					working = true;
-
-					$scope.data.i_matrix = [];
-					n.forEach((row) => {
-						let f = [];
-						for (var i = 1; i <= $scope.points.count; i++)
-							f.push(row.indexOf(i) != -1 ? 1 : 0)
-						$scope.data.i_matrix.push(f)
-					});
-
-					working = false;
-				}
+				src: '/parts/adjacency_list.html'
 			}
 		]
 	};
@@ -90,23 +31,51 @@ app.controller('DeskCtrl', ($scope) => {
 		a_list: []
 	};
 
-	let first_time = true;
+	$scope.generate = () => {
+		$scope.data.i_matrix = [
+			[0, 2, 0],
+			[0, 1, -1],
+			[-1, 1, 0],
+			[1, -1, 0],
+		];
+		let list = [];
+		for (let i = 0; i < $scope.points.count; i++)
+			list[i] = [];
+		$scope.data.i_matrix.forEach((row, r) => {
+			let from, to;
+			row.forEach((item, col) => {
+				let c = col;
+				if (!item)
+					return;
+				if (item == 2)
+					list[c].push(c);
+				else if (item == -1)
+					if (typeof to == 'undefined')
+						from = c;
+					else
+						list[c].push(to);
+				else if (item == 1)
+					if (typeof from == 'undefined')
+						to = c;
+					else
+						list[from].push(c)
+			})
+		});
+		$scope.data.a_matrix = [];
+		list.forEach((row, key) => {
+			$scope.data.a_list[key] = row;
+			let f = [];
+			for (let i = 0; i < $scope.points.count; i++)
+				f.push(row.indexOf(i) != -1 ? 1 : 0)
+			$scope.data.a_matrix.push(f);
+		});
+	};
+
 	$scope.$watch('points.count', (n, o) => {
-		if (first_time) {
-			first_time = false;
-			$scope.input_types.list[1].generate();
-		} else {
-			if (n > o) {
-				$scope.data.i_matrix.push(Array.apply(null, new Array(n)).map(() => 0))
-			} else {
-				$scope.data.i_matrix.pop();
-			}
-		}
 		$scope.points.array = Array.apply(null, new Array(n)).map(() => 0);
+		$scope.generate();
 	});
-	['data.a_matrix', 'data.i_matrix', 'data.a_list'].forEach((item, index) => {
-		$scope.$watch(item, $scope.input_types.list[index].watch, true);
-	});
+
 	$scope.a_list_add = (row, item) => {
 		$scope.data.a_list[row].push(item);
 		$scope.data.a_list[row].sort();
@@ -120,17 +89,7 @@ app.controller('DeskCtrl', ($scope) => {
 	};
 
 	$scope.a_matrix_remove = (u) => {
-		let x = JSON.stringify($scope.data.a_matrix[u].map((i) => i ? 1 : 0));
-		let foundIt = false;
-		$scope.data.i_matrix.forEach((item, index) => {
-			if (foundIt) return;
-			if (JSON.stringify(item) == x) {
-				$scope.data.i_matrix[index].forEach((i, j) => {
-					$scope.data.i_matrix[index][j] = 0;
-				});
-				foundIt = true;
-			}
-		})
+
 	};
 
 	$scope.a_matrix_add = () => {
@@ -140,6 +99,13 @@ app.controller('DeskCtrl', ($scope) => {
 	$scope.missingInList = (row, item) => {
 		return $scope.data.a_list[row].indexOf(item) == -1;
 	};
+
+	$scope.humanAList = (row) => {
+		let a = $scope.data.a_list[row].slice();
+		a = a.sort().map((i) => i + 1);
+		a.push(0);
+		return a.join('_');
+	}
 });
 
 const rand = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
