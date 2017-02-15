bb5cfc332dab6f8c4d25be5e93df81746d399336
Igor Leahu <igor.leahu24@gmail.com>
2017-02-15 20:38:46 +0200

    a_list 2 all

--- public/app.js
+++ public/app.js
@@ -33,45 +33,22 @@ app.controller('DeskCtrl', ($scope) => {
 
 	$scope.generate = () => {
 		$scope.data.i_matrix = [
-			[0, 2, 0],
-			[0, 1, -1],
-			[-1, 1, 0],
-			[1, -1, 0],
+		 [0, 2, 0],
+		 [0, 1, -1],
+		 [-1, 1, 0],
+		 [1, -1, 0],
+		 ];
+		 helper.iMatrix2All();
+
+		$scope.data.a_list = [
+			[1, 2],
+			[0, 1],
+			[1]
 		];
-		let list = [];
-		for (let i = 0; i < $scope.points.count; i++)
-			list[i] = [];
-		$scope.data.i_matrix.forEach((row, r) => {
-			let from, to;
-			row.forEach((item, col) => {
-				let c = col;
-				if (!item)
-					return;
-				if (item == 2)
-					list[c].push(c);
-				else if (item == -1)
-					if (typeof to == 'undefined')
-						from = c;
-					else
-						list[c].push(to);
-				else if (item == 1)
-					if (typeof from == 'undefined')
-						to = c;
-					else
-						list[from].push(c)
-			})
-		});
-		$scope.data.a_matrix = [];
-		list.forEach((row, key) => {
-			$scope.data.a_list[key] = row;
-			let f = [];
-			for (let i = 0; i < $scope.points.count; i++)
-				f.push(row.indexOf(i) != -1 ? 1 : 0)
-			$scope.data.a_matrix.push(f);
-		});
+		helper.aList2All();
 	};
 
-	$scope.$watch('points.count', (n, o) => {
+	$scope.$watch('points.count', (n) => {
 		$scope.points.array = Array.apply(null, new Array(n)).map(() => 0);
 		$scope.generate();
 	});
@@ -79,6 +56,7 @@ app.controller('DeskCtrl', ($scope) => {
 	$scope.a_list_add = (row, item) => {
 		$scope.data.a_list[row].push(item);
 		$scope.data.a_list[row].sort();
+		helper.aList2All();
 	};
 
 	$scope.a_list_remove = (row, item) => {
@@ -105,7 +83,110 @@ app.controller('DeskCtrl', ($scope) => {
 		a = a.sort().map((i) => i + 1);
 		a.push(0);
 		return a.join('_');
+	};
+
+	$scope.event = {
+		change: {
+			i_matrix: () => {
+				let invalidRows = [];
+				$scope.data.i_matrix.forEach((row, key) => {
+					let count = 0;
+					row.forEach((item) => {
+						if (!item)
+							return;
+						if (item == 2) count += 2;
+						else if (item == -1 || item == 1) count++;
+					});
+					if (count != 2)
+						invalidRows.push(key);
+				});
+				if (invalidRows.length)
+					console.log(invalidRows);
+				else {
+					helper.iMatrix2All();
+				}
+			}
+		},
+		random: {
+			i_matrix_add_row: () => {
+				$scope.data.i_matrix.push(Array.apply(null, new Array($scope.points.count)).map(() => 0))
+			},
+			i_matrix_remove_row: (u) => {
+				$scope.data.i_matrix.splice(u, 1);
+				$scope.event.change.i_matrix();
+			}
+		}
+	};
+
+	const helper = {
+		aMatrix2All: () => {
+
+		},
+		iMatrix2All: () => {
+			let list = iMatrix2List($scope.data.i_matrix, $scope.points.count);
+			$scope.data.a_matrix = [];
+			list.forEach((row, key) => {
+				$scope.data.a_list[key] = row;
+				let f = [];
+				for (let i = 0; i < $scope.points.count; i++)
+					f.push(+(row.indexOf(i) != -1))
+				$scope.data.a_matrix.push(f);
+			});
+		},
+		aList2All: () => {
+			let arcs = [];
+			$scope.data.a_list.forEach((row, key) => {
+				let f = [];
+				for (let i = 0; i < $scope.points.count; i++)
+					f.push(+(row.indexOf(i) != -1))
+				$scope.data.a_matrix.push(f);
+
+				row.forEach((item) => {
+					arcs.push([key, item])
+				})
+			});
+			$scope.data.i_matrix = [];
+			arcs.forEach((arc) => {
+				let f = [];
+				for (let i = 0; i < $scope.points.count; i++)
+					if (arc[0] == arc[1] && arc[0] == i)
+						f.push(2);
+					else if (arc[0] == i)
+						f.push(-1);
+					else if (arc[1] == i)
+						f.push(1);
+					else
+						f.push(0);
+				$scope.data.i_matrix.push(f);
+			})
+		},
 	}
 });
 
-const rand = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+const iMatrix2List = (data, points) => {
+	let list = [];
+	for (let i = 0; i < points; i++)
+		list[i] = [];
+	data.forEach((row) => {
+		let from, to;
+		row.forEach((item, col) => {
+			let c = col;
+			if (!item)
+				return;
+			if (item == 2)
+				list[c].push(c);
+			else if (item == -1)
+				if (typeof to == 'undefined')
+					from = c;
+				else
+					list[c].push(to);
+			else if (item == 1)
+				if (typeof from == 'undefined')
+					to = c;
+				else
+					list[from].push(c)
+		})
+	});
+	list = list.map((row) => Array.from(new Set(row)));
+	return list;
+};
